cd6b2461d133ce5cffefca844c71404f7f2e524e
omcoahm <omer.ahmetagic@gmail.com>
2023-08-17 11:05:41 +0200

    changed checkout code to v3 for github actions

--- conftest.py
+++ conftest.py
@@ -1,8 +1,11 @@
 import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import Page, BrowserContext
 from core.page_factory import PageFactory
 
 @pytest.fixture(scope="function")
-def pf(page: Page):
+def pf(page: Page, context: BrowserContext, request):
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page_factory = PageFactory(page)
     yield page_factory
+    if request.session.testsfailed != 0:
+        context.tracing.stop(path = f"report/{request.node.name.split('[')[0]}.zip")
